813e4784d85d1a9404fddaa182b48aceca0e09e0
Thomas Nendick <tjnendick5@gmail.com>
2026-06-13 21:17:50 -0700

    Standings: add GP pill; remove broadcast row from box scores; sw v22

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
  * Bump CACHE_VERSION on any breaking SW change to evict old caches.
  */
 
-const CACHE_VERSION  = 'wcb-v21-2026-06-13';
+const CACHE_VERSION  = 'wcb-v22-2026-06-13';
 const STATIC_CACHE   = `${CACHE_VERSION}-static`;
 const EXTERNAL_CACHE = `${CACHE_VERSION}-external`;
 
